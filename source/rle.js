caef79270c1d8c6b30f34e6d528010f2fec07736
Yura <yura11011@ya.ru>
2021-09-01 14:58:12 +0300

    старая функция переименована, написана новая функция без регулярных выражений

--- source/rle.js
+++ source/rle.js
@@ -6,6 +6,22 @@
   * @param {string} string - исходная строка
   * @returns {string}
   */
-const rle = (string) => (typeof string != 'string') ? ''
-	:string.replace(/(.)\1+/g, (series, char) => (char + series.length));
+const rleRegExp = (string) => (typeof string != 'string') ? ''
+    :string.replace(/(.)\1+/g, (series, char) => (char + series.length));
 
+const rle = (string) => {
+    if (typeof string != 'string') {
+        return '';
+    }
+    let compressed = '';
+    let rep_count = 0;
+    for (let i = 0; i < string.length; ++i) {
+        if (string[i] == string[i+1]) {
+            rep_count++;
+        } else {
+            compressed += string[i] + (rep_count > 0 ? rep_count + 1 : '');
+            rep_count = 0;
+        }
+    }
+    return compressed;
+}
