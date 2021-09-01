851d926d4dd890cefa771bd775ce830ddb963da9
Yura <yura11011@ya.ru>
2021-09-01 13:24:20 +0300

    test names and format fixed

--- tests/rle.js
+++ tests/rle.js
@@ -1,17 +1,22 @@
 ﻿'use strict';
 
 QUnit.module('Тестируем функцию rle', function () {
-	QUnit.test('rle работает правильно', function (assert) {
+	QUnit.test('rle работает правильно для стандартных данных', function (assert) {
 		assert.strictEqual(rle('AAAB'), 'A3B');
 		assert.strictEqual(rle('BCCDDDAXXXX'), 'BC2D3AX4');
 		assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
 	});
 
-	QUnit.test('rle снова работает правильно', function (assert) {
+	QUnit.test('rle работает с пустой строкой', function (assert) {
 		assert.strictEqual(rle(''), '');
-		assert.strictEqual(rle('AB'), 'AB');	
-            assert.strictEqual(rle('BABB333'), 'BAB233');
+	});
+	
+	QUnit.test('rle работает, ничего не сжимая', function (assert) {
+		assert.strictEqual(rle('AB'), 'AB');
+	});
+	
+	QUnit.test('rle работает, когда на входе есть числа', function (assert) {
+		assert.strictEqual(rle('BABB333'), 'BAB233');
 	});
 });
-
 
