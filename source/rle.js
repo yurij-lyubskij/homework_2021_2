2b36da8e39d61a1fd4d9b63b18f8b918cdc97288
Yura <yura11011@ya.ru>
2021-09-01 13:10:52 +0300

    JSDoc и первая стрелочная функция

--- source/rle.js
+++ source/rle.js
@@ -1,7 +1,10 @@
 'use strict';
-
-function rle(string)
-{
-  return string.replace(/(.)\1+/g, function(a, b) {return b + a.length});
-}
+	
+/**
+  * Сжимаем строку
+  * 
+  * @param {string} string - исходная строка
+  * @returns {string}
+  */
+const rle = (string) => string.replace(/(.)\1+/g, function(a, b) {return b + a.length});
 
